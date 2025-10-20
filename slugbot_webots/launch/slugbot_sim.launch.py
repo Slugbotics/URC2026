19256ef377a16847df23db78d52dee371e44382d
Kyle-Eldridge <keldridge07@gmail.com>
2025-10-20 11:06:01 -0700

    Make example_controller work

--- slugbot_webots/launch/slugbot_sim.launch.py
+++ slugbot_webots/launch/slugbot_sim.launch.py
@@ -1,11 +1,16 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    package_dir = get_package_share_directory('slugbot_webots')
+    world_path = os.path.join(package_dir, 'worlds', 'slugbot_world.wbt')
+    
     return LaunchDescription([
         ExecuteProcess(
-            cmd=['webots', '--world', '/root/slugbotics_ws/src/2025-2026-URC/slugbot_webots/worlds/slugbot_world.wbt'],
+            cmd=['webots', world_path],
             output='screen'
         ),
         Node(
